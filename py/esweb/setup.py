f01ee2929eb35d246a1f04692ede73584c1d7bd5
tim <tim@sy-edm.com>
2018-07-16 12:35:20 +0400

    With Flask Files now added

---
+++ py/esweb/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='esweb',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='tim',
+    author_email='tim@sy-edm.com',
+    description='Flask ElasticSearch Query and Render',
+    install_requires=[
+            'flask',
+            'wtforms',
+            'elasticsearch',
+            'markdown',
+    ],
+)
