5fd75b774579ed2fb8236efeb75821886a2762c0
Tim Seed <tim@sy-edm.com>
2018-07-23 14:59:12 +0400

    File and JSON at the same time

--- py/esweb/MixedContent.sh
+++ py/esweb/MixedContent.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#Adding Binary file - this index will be a 1 up Numbering system
 #curl --header "Content-Type: multipart/mixed" \
 #     -X POST http://127.0.0.1:5000/add \
 #     --data-binary @MixedContent.sh
@@ -9,14 +10,20 @@
 # Note 2 variables file and json
 # file is loaded using < redirection
 # Json_Data is a hand crafted dictionary
-#curl -X POST -H "Content-Type: multipart/form-data" \
-#     -F "file=<MixedContent.sh" \
-#     -F "json_data={\"id\":\"MixedContent.sh\",\"security\":\"None\"}" \
-#     -X POST http://127.0.0.1:5000/add
-
+# This will create an index using a filename
 cd "/Users/timothyhseed/Library/Mobile Documents/com~apple~CloudDocs/Markdown"
 find . | grep md$ | xargs -I {} \
     curl -X POST -H "Content-Type: multipart/form-data" \
          -F "file=<{}" \
-         -F "json_data={\"id\":\"data_file1\",\"security\":\"None\"}" \
+         -F "json_data={\"id\":\"$(basename {})\",\"security\":\"None\"}" \
          -X POST http://0.0.0.0:5000/add
+
+
+Which means in the future you can just apply "deltas"
+
+i.e.
+
+    find -ctime -7
+    find -mtime -7 
+
+
